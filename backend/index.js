6e1210ad85bf8e8834c3623a10c66fc7810623a4
Sowre Rajan <sowrirajanofficial@gmail.com>
2021-09-24 23:39:07 +0530

    register and login routes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
+const authRoute = require("./routes/auth");
 
+app.use(express.json());
+app.use(morgan("dev"));
 mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
@@ -11,6 +14,8 @@ mongoose
   })
   .then(() => console.log("Connected to database"))
   .catch((err) => console.log("err......", err));
+
+app.use("/api/auth", authRoute);
 
 app.listen(5000, () => {
   console.log("Listening on port 5000");
