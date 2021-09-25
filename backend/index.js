cd06418e60637b82f21f1b4c1c113500c02b5121
Sowre Rajan <sowrirajanofficial@gmail.com>
2021-09-25 16:10:22 +0530

    user and post CRUD's

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const authRoute = require("./routes/auth");
-
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 app.use(express.json());
 app.use(morgan("dev"));
 mongoose
@@ -16,7 +17,8 @@ mongoose
   .catch((err) => console.log("err......", err));
 
 app.use("/api/auth", authRoute);
-
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.listen(5000, () => {
   console.log("Listening on port 5000");
 });
